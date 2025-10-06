f380f58b50c1009599db7df4936fd74416a7621f
Omkar <omkaryelsange1010@gmail.com>
2025-10-06 15:15:04 +0530

    UseState UseEffect Conditional Renderning and other things implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import "../App.css";
 
 const Navbar = () => {
+  const [btnName, setBtnName] = useState("LIGHT");
+
   return (
     <div className="navbar">
       <h1>LOGO</h1>
@@ -9,6 +12,20 @@ const Navbar = () => {
         <li>WOMENS</li>
         <li>KIDS</li>
         <li>CART</li>
+        <button
+          style={{
+            background: "purple",
+            color: "white",
+            padding: "10px",
+            borderRadius: "10px",
+            border: "1px solid black",
+          }}
+          onClick={() => {
+            btnName === "LIGHT" ? setBtnName("DARK") : setBtnName("LIGHT");
+          }}
+        >
+          {btnName}
+        </button>
       </ul>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
-import ProductList from "../utils/ProductList";
+// import { ProductList } from "../Data/ProductList";
+import { useEffect } from "react";
+import Skeleton from "./Skeleton";
 
 const ProductCard = () => {
-  return (
-    <div className="Product_Card">
-      {ProductList.map((ProductList) => {
-        return <Product key={ProductList.id} ProductList={ProductList} />;
-      })}
+  // const [TopProducts, setTopProducts] = useState(ProductList);
+  const [TopProducts, setTopProducts] = useState([]);
+  const [btnName, setBtnName] = useState("Top Rated Products");
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await data.json();
+    setTopProducts(jsonData);
+  };
+
+  return TopProducts.length === 0 ? (
+    <Skeleton />
+  ) : (
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div
+          style={{
+            margin: "10px",
+            display: "flex",
+            gap: "2px",
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            placeholder="Search Product"
+            value={searchText}
+            style={{
+              padding: "10px",
+              borderRadius: "20px",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          />
+          <button
+            style={{ padding: "10px", borderRadius: "20px" }}
+            onClick={() => {
+              console.log(searchText);
+              const filteredProduct = TopProducts.filter((ProductList) => {
+                return ProductList.title.includes(searchText.toLowerCase());
+              });
+              setTopProducts(filteredProduct);
+              setSearchText("");
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          style={{
+            margin: "10px",
+            background: "purple",
+            color: "white",
+            padding: "10px",
+            borderRadius: "20px",
+            border: "1px solid black",
+          }}
+          onClick={() => {
+            const filteredProduct = TopProducts.filter(
+              (ProductList) => ProductList.rating.rate >= 4
+            );
+
+            setTopProducts(filteredProduct);
+            btnName === "Top Rated Products"
+              ? setBtnName("All Products")
+              : setBtnName("Top Rated Products");
+
+            if (btnName === "All Products") {
+              fetchData();
+            }
+          }}
+        >
+          {btnName}
+        </button>
+      </div>
+      <div className="Product_Card">
+        {TopProducts.map((ProductList) => {
+          return <Product key={ProductList.id} ProductList={ProductList} />;
+        })}
+      </div>
     </div>
   );
 };
