f754a1294c7d69b66e0c70f1e42999e9593c2a3c
Omkar <omkaryelsange1010@gmail.com>
2025-10-11 12:17:25 +0530

    adding lifting state up functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex items-center justify-center text-center p-[10px] bg-purple-700 text-white w-full mt-[40px]">
-      <h3>Made with ❤️ by Omkar Yelsange</h3>
+      <span>Made with ❤️ by Omkar Yelsange</span>
     </div>
   );
 };
